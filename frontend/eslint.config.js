974a26230fc8e0fd2ceb9cd5e507daabbe5bcaac
Olha Fursova <teor15manypeople1@gmail.com>
2026-08-21 02:31:42 +0200

    CHANGE globals property for using FormData

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import prettier from "eslint-config-prettier";
+import globals from "globals";
 
 export default [
   { ignores: ["dist", "node_modules"] },
@@ -12,7 +13,7 @@ export default [
       ecmaVersion: "latest",
       sourceType: "module",
       parserOptions: { ecmaFeatures: { jsx: true } },
-      globals: { window: "readonly", document: "readonly" },
+      globals: globals.browser,
     },
     plugins: { react, "react-hooks": reactHooks },
     rules: {
