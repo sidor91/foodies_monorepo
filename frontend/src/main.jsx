c42cbe32fb7c8c30c19a09da1a6e9670ce0a4978
Eduard Bolma <edaurd.bolma@gmail.com>
2026-08-26 20:46:40 +0200

    conflicts resovled, added persistGate

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./components/App/App.jsx";
 import { Provider } from "react-redux";
-import { store } from "../redux/store.js";
+import { persistor, store } from "../redux/store.js";
+
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>,
