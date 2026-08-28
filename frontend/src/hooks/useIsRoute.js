7b62ec3286fa99fcc7b52fc10bccce914f0389da
Olha Fursova <teor15manypeople1@gmail.com>
2026-08-28 17:42:46 +0200

    FIX useIsRoute x2

--- frontend/src/hooks/useIsRoute.js
+++ frontend/src/hooks/useIsRoute.js
@@ -2,10 +2,13 @@ import { useLocation } from "react-router-dom";
 
 const useIsRoute = (path) => {
   const { pathname } = useLocation();
+  if (path === "/") {
+    return pathname === path;
+  }
+  const rootPathName = pathname.split("/")[1];
+  const routePath = path.split("/")[1];
 
-  const rootPath = `/${pathname.split("/")[1]}`;
-
-  return rootPath === path;
+  return rootPathName === routePath;
 };
 
 export default useIsRoute;
